7400d6da17ac5ffe279a1fbb45a432563f280677
Nyorikenoichi <malsef.maks@yandex.ru>
2021-03-25 23:16:27 +0300

    Create main template

---
+++ Character.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Our_cool_game
+{
+    class Character
+    {
+        //public uint ID  //должно быть уникальным
+        public string Name { get; private set; }
+        //public enum state { get; set; }
+        public bool CanTalk { get; set; }
+        public bool CanMove { get; set; }
+        //public enum race { get; private set; }
+        //public enum gender { get; private set; }
+        public uint Age { get; set; }
+        public uint CurrentHP { get; set; }
+        public uint MaxHP { get; set; }
+        public uint XP { get; set; }
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Our_cool_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
