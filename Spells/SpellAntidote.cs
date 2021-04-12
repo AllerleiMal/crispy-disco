e23e6d1b5f606e96f6fe314143c8adb33dee6c98
togramort <togramort@gmail.com>
2021-04-13 00:33:11 +0300

    add spells cure and antidote

---
+++ Spells/SpellAntidote.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using OurCoolGame.Enums;
+
+namespace OurCoolGame.Spells
+{
+    class SpellAntidote : Spell
+    {
+        public override int ManaCost { get; protected set; } = 30;
+        public override bool Gesturing { get; protected set; } = false;
+        public override bool Pronouncing { get; protected set; } = false;
+
+
+        public override void MagicEffect(Wizard origin, Wizard target)
+        {
+            if (origin.CurMana < ManaCost)
+            {
+                Console.WriteLine("You don't have enough mana to give this character antidote");
+            }
+
+            if (target.CharacterState != State.Poisoned)
+            {
+                Console.WriteLine("Character must be poisoned");
+            }
+            
+            if (target.CharacterState == State.Dead)
+            {
+                Console.WriteLine("You can't apply it to the dead character");
+            }
+
+            target.CharacterState = target.CurrentHealthPoints <= target.MaxHealthPoints / 10 ? State.Weakened : State.Healthy;
+            
+            origin.CurMana -= ManaCost;
+        }
+    }
+}
