fb3d9163e96bb496bbd42713c46b61ffdd6a5346
Pavel Emelyanenko <emelpavel2002@gmail.com>
2021-04-16 17:54:36 +0300

    Create first part of training

--- GameLogic.cs
+++ GameLogic.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
 using OurCoolGame.Artefacts;
 using OurCoolGame.Enums;
 
@@ -9,6 +10,7 @@ namespace OurCoolGame
     {
         public static int MoveCounter { get; set; } = 0;
         private Wizard _mainPlayer;
+
         private int _difficultyLevel;
         // private List<Artefact> _allArtefacts; 
 
@@ -138,7 +140,7 @@ namespace OurCoolGame
 
             Console.WriteLine(
                 "And now is the time to choose a subclass of you character:\nWizard(1)\nWarrior(2)\nBandit(3)");
-            Console.ReadKey();
+            Console.ReadLine();
             Console.WriteLine(
                 "Ooops... There is a problem. Whatever you choose, your subclass will be a wizard. The rest are not finalized yet, the game is on the prerelease beta gamma alpha test 0.0.0.0.1a.");
 
@@ -149,34 +151,131 @@ namespace OurCoolGame
         //that is a method that would be called with !help, it shows information about basic game commands
         public void ShowInformationAboutCommands()
         {
-            //add other commands
-            Console.WriteLine(
-                "!help - get info about commands\n!new_game - will start the game from the very beginning\nUSE SPELL [number of the spell in your spell list] TO [target character] WITH [spell power] - ");
+            string temp;
+            //maybe while true should be deleted
+            while (true)
+            {
+                temp = Console.ReadLine();
+                if (temp != "!help")
+                {
+                    continue;
+                }
+
+                //add other commands
+                Console.WriteLine(
+                    "!help - get info about commands\n!new_game - will start the game from the very beginning\nUSE SPELL [number of the spell in your spell list] TO [target character] WITH [spell power] - ...\n!inventory - to see your artefacts");
+                break;
+            }
+        }
+
+        public void ShowRules()
+        {
+            //there should be basic rules
         }
 
         public void GenerateLevel()
         {
-            if (_difficultyLevel == 0)
+            switch (_difficultyLevel)
             {
-                
-            }
+                case 0:
+                {
+                    RunTraining();
+                    break;
+                }
+                case <= 3:
+                {
+                    //RunEasyLevel();
+                    break;
+                }
+                case > 3 and <= 6:
+                {
+                    //RunMediumLevel();
+                    break;
+                }
+                case > 6 and <= 10:
+                {
+                    // RunHardLevel();
+                    if (_difficultyLevel == 10)
+                    {
+                        // RunFinalPlot();
+                    }
 
-            if (_difficultyLevel <= 3)
-            {
-                
+                    break;
+                }
             }
+        }
 
-            if (_difficultyLevel is > 3 and <= 6)
+        //this method is for creating a basic arena with 1 enemy with training messages
+        private void RunTraining()
+        {
+            ++_difficultyLevel;
+            _enemy.Add(new Wizard("dummy", Race.Human, Gender.Undefined, 10));
+            Console.WriteLine(
+                "Hello, exile! That is your first fight. Your enemy is {0}. Now we are going to train not to suck in the real fight.\nThere is something interesting in your bag, check it(enter \"!inventory\")",
+                _enemy[0].Name);
+            _mainPlayer.PickUpArtefact(new LightningStaff(150));
+            while (true)
             {
-                
+                var temp = Console.ReadLine();
+                if (temp == "!inventory")
+                {
+                    _mainPlayer.ShowInventory();
+                    break;
+                }
+                Console.WriteLine("Something went wrong, try again and follow the right command's format");
             }
+            Console.WriteLine(
+                "Good job! Now you can see what is in your bag. Choose one of the artefact and use it on your enemy.\n Ah, ye... You don't know how. Enter \"!help\"");
+            Thread.Sleep(2000);
+            ShowInformationAboutCommands();
+            Console.WriteLine("Ok, now you know more and can do something");
+            Thread.Sleep(2000);
+            Console.WriteLine("Try your artefact on the {0}", _enemy[0].Name);
+            while (true)
+            {
+                var temp = Console.ReadLine();
+                if (temp == "USE ARTEFACT 1 TO dummy")
+                {
+                    _mainPlayer.UseArtefact(_mainPlayer._inventory[0], _enemy[0]);
+                    break;
+                }
+                Console.WriteLine("Something went wrong, try again and follow the right command's format");
+            }
+            Console.WriteLine("Good! Let's check, what happened with our dummy");
+            Thread.Sleep(2000);
+            Console.WriteLine("{0}/{1}", _enemy[0].CurrentHealthPoints, _enemy[0].MaxHealthPoints);
+            Thread.Sleep(2000);
+            Console.WriteLine("Now we would check how you can take damage");
+            Thread.Sleep(2000);
+            _enemy[0]._inventory.Add(new LightningStaff(1500));
+            Thread.Sleep(2000);
+            _enemy[0].UseArtefact(_enemy[0]._inventory[0], _mainPlayer);
+            Thread.Sleep(2000);
+            Console.WriteLine("Ouffff, your defence is really weak {0}/{1} HP", _mainPlayer.CurrentHealthPoints, _mainPlayer.MaxHealthPoints);
+            Thread.Sleep(2000);
+            _mainPlayer.PickUpArtefact(new LivingWater(BottleSize.Small));
+            Thread.Sleep(2000);
+            _mainPlayer.PickUpArtefact(new DeadWater(BottleSize.Small));
+            Thread.Sleep(2000);
+            Console.WriteLine("You have 2 special bottles. Living is for live regeneration and dead is for mana regeneration.\nThey disappoint after using, so think twice and don't use it when you are full. Now restore your HP");
+            while (true)
+            {
+                var temp = Console.ReadLine();
+                if (temp == "USE ARTEFACT 2 TO me")
+                {
+                    _mainPlayer.UseArtefact(_mainPlayer._inventory[1], _mainPlayer);
+                    break;
+                }
+                Console.WriteLine("Something went wrong, try again and follow the right command's format");
+            }
+            Console.WriteLine("Now it is better, {0}/{1} HP", _mainPlayer.CurrentHealthPoints, _mainPlayer.MaxHealthPoints);
 
-            if (_difficultyLevel is > 6 and <= 10)
-            {
-                
-                
-                if 
-            }
+            /*
+             * here should be same thing for spells, as it is for artefacts
+             *
+             *
+             * after demonstration work of spells, use Dead Water as it was done with living water
+             */
         }
     }
 }
